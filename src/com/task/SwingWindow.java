e7d73543094dc3cf014f3d07b838ee08913cadef
Alexander Tolich <aleksandrtolich@gmail.com>
2018-11-10 12:55:46 +0300

    add new feature to client side (generate group matrixes, random renumbering, restore after renumbering)

--- src/com/task/SwingWindow.java
+++ src/com/task/SwingWindow.java
@@ -1,12 +1,12 @@
 package com.task;
 
+import com.task.composites.HorizontalMatrixGroup;
+import com.task.composites.VerticalMatrixGroup;
+import com.task.decorators.RenumberingDecorator;
 import com.task.drawers.ConsoleDrawer;
 import com.task.drawers.IDrawer;
 import com.task.drawers.SwingDrawer;
-import com.task.matrixes.AbstractMatrix;
-import com.task.matrixes.IMatrix;
-import com.task.matrixes.NormalMatrix;
-import com.task.matrixes.SparseMatrix;
+import com.task.matrixes.*;
 import com.task.utils.InitiatorMatrix;
 import javafx.util.Pair;
 
@@ -14,16 +14,25 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 import static com.task.utils.SwingUtils.deleteAll;
 
 public class SwingWindow extends JFrame {
     private JButton generateSimpMatrixButton = new JButton("Generate the simple matrixes");
     private JButton generateSparMatrixButton = new JButton("Generate the sparse matrixes");
+    private JButton generateVertGroupMatrixesButton = new JButton("Generate the V group");
+    private JButton restoreButton = new JButton("Restore");
+    private JButton renumberButton = new JButton("Renumber");
     private JCheckBox jCheckBox = new JCheckBox("enable border", true);
     private JComboBox<Pair<Integer, Integer>> comboBox = new JComboBox<>();
     private NormalMatrix<Integer> normalMatrix;
     private SparseMatrix<Integer> sparseMatrix;
+    private VerticalMatrixGroup vGroup;
+    private AMatrixBridge<Integer> lastMatrixDraw;
+    private RenumberingDecorator decorator;
     private int rows = 20;
     private int cols = 20;
     private Container gridContainer;
@@ -40,12 +49,18 @@ public class SwingWindow extends JFrame {
         Box panel = Box.createHorizontalBox();
         panel.add(generateSimpMatrixButton);
         panel.add(generateSparMatrixButton);
+        panel.add(generateVertGroupMatrixesButton);
+        panel.add(restoreButton);
+        panel.add(renumberButton);
         panel.add(comboBox);
         panel.add(jCheckBox);
         rootContainer.add(panel);
         rootContainer.add(Box.createVerticalStrut(5));
         generateSimpMatrixButton.addMouseListener(new GenerateNormalHandler());
         generateSparMatrixButton.addMouseListener(new GenerateSparseHandler());
+        generateVertGroupMatrixesButton.addMouseListener(new GenerateVGroupHandler());
+        renumberButton.addMouseListener(new RenumberMatrixHandler());
+        restoreButton.addMouseListener(new RestoreFromRenumberingMatrixHandler());
 
         gridContainer = new Panel();
         rootContainer.add(gridContainer);
@@ -81,14 +96,72 @@ public class SwingWindow extends JFrame {
         InitiatorMatrix.randomFillMatrix(sparseMatrix, rows * 2, 1100);
     }
 
-    public void printMatrixOnSwingWindow(AbstractMatrix<Integer> mat, boolean enableBorder) {
+    private void initVertGroupMatrix(){
+        updateRowsCols();
+        vGroup = new VerticalMatrixGroup(null);
+        List<Integer> partsW = divideSizeIntoParts(cols, 3);
+        List<Integer> partsH = divideSizeIntoParts(rows, 3);
+        HorizontalMatrixGroup wGroup = new HorizontalMatrixGroup(null);
+
+        AMatrixBridge<Integer> m1W = new SparseMatrix<>(partsH.get(0), partsW.get(0), 0);
+        InitiatorMatrix.randomFillMatrix(m1W, partsH.get(0) * 2, 1100);
+
+        AMatrixBridge<Integer> m2W = new NormalMatrix<>(partsH.get(0), partsW.get(1), 0);
+        InitiatorMatrix.randomFillMatrix(m2W, partsH.get(0) * 2, 1100);
+
+        AMatrixBridge<Integer> m3W = new SparseMatrix<>(partsH.get(0), partsW.get(2), 0);
+        InitiatorMatrix.randomFillMatrix(m3W, partsH.get(0) * 2, 1100);
+
+        wGroup.add(m1W); wGroup.add(m2W); wGroup.add(m3W);
+
+        AMatrixBridge<Integer> m2H = new SparseMatrix<>(partsH.get(1), partsW.get(0) + partsW.get(1), 0);
+        InitiatorMatrix.randomFillMatrix(m2H, partsH.get(1) * 2, 1100);
+
+        AMatrixBridge<Integer> m3H = new NormalMatrix<>(partsH.get(2), partsW.get(0) + partsW.get(2), 0);
+        InitiatorMatrix.randomFillMatrix(m3H, partsH.get(2) * 2, 1100);
+
+        vGroup.add(wGroup); vGroup.add(m2H); vGroup.add(m3H);
+    }
+
+    private List<Integer> divideSizeIntoParts(int size, int numParts){
+        List<Integer> sizeParts = new ArrayList<>();
+        if(numParts > size) {
+            sizeParts.add(size);
+            return sizeParts;
+        }
+        int tmpSize = 0;
+        double div = new BigDecimal(size).divide(new BigDecimal(numParts), BigDecimal.ROUND_HALF_UP).doubleValue();
+        int divI = (int) div;
+        for(int i = 0; i < numParts; i++){
+            if(tmpSize + divI <= size){
+                tmpSize += divI;
+                sizeParts.add(divI);
+            }
+
+            if(i == numParts - 1 && tmpSize + divI > size){
+                sizeParts.add(size - tmpSize);
+                tmpSize += size - tmpSize;
+            }
+        }
+
+        int sum = 0;
+        for(Integer i : sizeParts){
+            sum += i;
+        }
+        if(sum != size){
+            throw new UnsupportedOperationException("check divide size into parts");
+        }
+        return sizeParts;
+    }
+
+    public void printMatrixOnSwingWindow(AMatrixBridge<Integer> mat, boolean enableBorder) {
         deleteAll(gridContainer);
         IDrawer<Integer> drawer = new SwingDrawer<>(gridContainer, enableBorder);
         mat.setDrawer(drawer);
         mat.draw();
     }
 
-    public void printMatrixOnConsole(AbstractMatrix<Integer> mat, boolean enableBorder) {
+    public void printMatrixOnConsole(AMatrixBridge<Integer> mat, boolean enableBorder) {
         IDrawer<Integer> drawer = new ConsoleDrawer<>(enableBorder);
         mat.setDrawer(drawer);
         mat.draw();
@@ -97,8 +170,9 @@ public class SwingWindow extends JFrame {
 
     private void onClick(IMatrix<Integer> matrix) {
         boolean enableBorder = jCheckBox.isSelected();
-        printMatrixOnConsole((AbstractMatrix<Integer>) matrix, enableBorder);
-        printMatrixOnSwingWindow((AbstractMatrix<Integer>) matrix, enableBorder);
+        printMatrixOnConsole((AMatrixBridge<Integer>) matrix, enableBorder);
+        printMatrixOnSwingWindow((AMatrixBridge<Integer>) matrix, enableBorder);
+        lastMatrixDraw = (AMatrixBridge<Integer>) matrix;
         rootContainer.revalidate();
         rootContainer.repaint();
         pack();
@@ -117,6 +191,42 @@ public class SwingWindow extends JFrame {
         public void mouseClicked(MouseEvent e) {
             initSparseMatrix();
             onClick(sparseMatrix);
+        }
+    }
+
+    class GenerateVGroupHandler extends MouseAdapter {
+        @Override
+        public void mouseClicked(MouseEvent e) {
+            initVertGroupMatrix();
+            onClick(vGroup);
+        }
+    }
+
+    class RenumberMatrixHandler extends MouseAdapter {
+        @Override
+        public void mouseClicked(MouseEvent e) {
+            if(lastMatrixDraw == null) return;
+            int i1 = (int) (Math.random() * rows);
+            int i2 = (int) (Math.random() * rows);
+            int j1 = (int) (Math.random() * cols);
+            int j2 = (int) (Math.random() * cols);
+            int rowsOrCols = (int) (Math.random() * 2);
+            if(rowsOrCols == 0){
+                decorator = new RenumberingDecorator(lastMatrixDraw, i1, i2, true);
+            } else {
+                decorator = new RenumberingDecorator(lastMatrixDraw, j1, j2, false);
+            }
+            onClick(decorator);
+        }
+    }
+
+    class RestoreFromRenumberingMatrixHandler extends MouseAdapter {
+        @Override
+        public void mouseClicked(MouseEvent e) {
+            if(decorator == null) return;
+            AMatrixBridge<Integer> sourceMat = decorator.getSourceObject();
+            onClick(sourceMat);
+            decorator = null;
         }
     }
 }
